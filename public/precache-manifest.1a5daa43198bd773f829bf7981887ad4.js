2af9fa12a41f02cbde7dcf9f1a9dc2be2ab38afe
Djvmil <ndieme16@hotmail.com>
2022-04-23 14:44:59 +0000

    first commit

---
+++ public/precache-manifest.1a5daa43198bd773f829bf7981887ad4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778b875075de09c4e9373648cc65b9a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0031a2e34d722bc62336",
+    "url": "/static/css/2.13f44941.chunk.css"
+  },
+  {
+    "revision": "e458f941f76200150e8c",
+    "url": "/static/css/main.f2733f9b.chunk.css"
+  },
+  {
+    "revision": "0031a2e34d722bc62336",
+    "url": "/static/js/2.ff842b1a.chunk.js"
+  },
+  {
+    "revision": "70c9bdf0768d7c2c376aa9d3dfe643a8",
+    "url": "/static/js/2.ff842b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e458f941f76200150e8c",
+    "url": "/static/js/main.fac71af4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ad1d103b778e53851d09ff5c804b1907",
+    "url": "/static/media/getFetch.ad1d103b.cjs"
+  },
+  {
+    "revision": "2098fcaa8e17280e32c4c5c5472eebf7",
+    "url": "/static/media/img1.2098fcaa.png"
+  },
+  {
+    "revision": "14aa19ca5dfe9cde66da17b1e74f1783",
+    "url": "/static/media/img2.14aa19ca.png"
+  },
+  {
+    "revision": "683fc92cc1f77573b407c41b3a6632f5",
+    "url": "/static/media/img4.683fc92c.png"
+  },
+  {
+    "revision": "eb29cc41c05d8d3bbd49ec3737dd7bce",
+    "url": "/static/media/rolling.eb29cc41.svg"
+  }
+]);
